76de6fc7a43ccaca0c8ce1cbcf531224d0cf77db
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-23 21:44:16 +0100

    Fixed typo with boolean operators

--- jenkinsScripts/result.py
+++ jenkinsScripts/result.py
@@ -2,9 +2,9 @@ from os import getenv
 s_one = getenv('STAGE_ONE')
 s_two = getenv('STAGE_TWO')
 
-if s_one == true and s_two == true:
+if s_one == True and s_two == True:
     print("El proyecto va viento en popa!!!")
-elif s_one == false and s_two == false:
+elif s_one == False and s_two == False:
     print( "Esto pinta muy mal")
 else:
     print("Alguna de las dos stages ha fallado")
