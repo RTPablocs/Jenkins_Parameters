5fd3ff116c4160950f753ce96a404ccce988297e
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-23 21:37:00 +0100

    Fixed package sys usage

--- jenkinsScripts/processA.py
+++ jenkinsScripts/processA.py
@@ -2,7 +2,7 @@ from sys import argv
 from os import system
 
 
-if sys.argv[0]%2 == 0:
+if argv[1]%2 == 0:
     os.system('export STAGE_ONE=true')
 else:
     os.system('export STAGE_ONE=true')
--- jenkinsScripts/processB.py
+++ jenkinsScripts/processB.py
@@ -2,7 +2,7 @@ from sys import argv
 from os import system
 
 
-if sys.argv[0]%2 == 0:
+if argv[1]%2 == 0:
     os.system('export STAGE_TWO=true')
 else:
     os.system('export STAGE_TWO=true')
