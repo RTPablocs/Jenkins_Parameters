57a4887f71c8ea6edf94568d763a5b686b67dbd0
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-23 21:41:37 +0100

    Fixed conversion

--- jenkinsScripts/processA.py
+++ jenkinsScripts/processA.py
@@ -2,7 +2,7 @@ from sys import argv
 from os import system
 
 
-if argv[1]%2 == 0:
+if int(argv[1])%2 == 0:
     os.system('export STAGE_ONE=true')
 else:
     os.system('export STAGE_ONE=true')
--- jenkinsScripts/processB.py
+++ jenkinsScripts/processB.py
@@ -2,7 +2,7 @@ from sys import argv
 from os import system
 
 
-if argv[1]%2 == 0:
+if int(argv[1])%2 == 0:
     os.system('export STAGE_TWO=true')
 else:
     os.system('export STAGE_TWO=true')
