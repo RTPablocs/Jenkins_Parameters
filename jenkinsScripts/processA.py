1ba407063b9f4acc099419b0b40913af9788d8a7
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-23 21:42:32 +0100

    fixed package issues

--- jenkinsScripts/processA.py
+++ jenkinsScripts/processA.py
@@ -3,6 +3,6 @@ from os import system
 
 
 if int(argv[1])%2 == 0:
-    os.system('export STAGE_ONE=true')
+    system('export STAGE_ONE=true')
 else:
-    os.system('export STAGE_ONE=true')
+    system('export STAGE_ONE=true')
